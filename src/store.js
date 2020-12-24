7b77f4318a03f91065b9bfbfcbcad9abc72f4246
Tommy Holmes <holmesiii.thomas@gmail.com>
2020-12-23 19:24:29 -0500

    added thunks and loading todos

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import { todos } from './todos/reducers'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { todos, isLoading } from './todos/reducers'
 
 const reducers = {
     todos,
+    isLoading
 };
 
 const persistConfig = {
@@ -17,5 +20,10 @@ const persistConfig = {
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const configureStore = () => createStore(persistedReducer);
+export const configureStore = () => createStore(
+    persistedReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    ),
+);
 
--- src/todos/TodoList.jsx
+++ src/todos/TodoList.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import { connect } from 'react-redux';
 import TodoListItem from './TodoListItem';
 import TodoForm from './TodoForm'
+import { loadTodos } from './thunks'
 import { removeTodo, completeTodo } from './actions';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletePressed }) => (
-    <div className="list-wrapper">
-        <TodoForm />
-        {todos.map(todo => <TodoListItem 
-            key={todo.text}
-            todo={todo}
-            onRemovePressed={onRemovePressed}
-            onCompletePressed={onCompletePressed}/>)}
-    </div>
-)
+const TodoList = ({ todos = [], onRemovePressed, onCompletePressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos()
+    }, []);
+
+    const loading = (<div>Loading Todos...</div>);
+    const content = (
+        <div className="list-wrapper">
+            <TodoForm />
+            {todos.map(todo => <TodoListItem 
+                key={todo.text}
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletePressed={onCompletePressed}/>)}
+        </div>
+    );
+    return isLoading ? loading : content;
+}
 
 const mapStateToProps = state => ({
-    todos: state.todos
+    todos: state.todos,
+    isLoading: state.isLoading
 });
 
 const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletePressed: text => dispatch(completeTodo(text))
 });
